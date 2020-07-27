720ec0056ba5e9bf45ee499bd7f8853731fb6977
huangyisan <ahuangyisan@live.cn>
2020-07-28 00:25:07 +0800

    update

--- tsushima-admin/src/views/layout.vue
+++ tsushima-admin/src/views/layout.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
-    主布局
-    <!-- children的路由 -->
-    <router-view></router-view>
+    <el-container class="fullscrean" style="overflow:hidden">
+      <el-header>Header</el-header>
+      <!-- header占了60px,所以侧边栏被占用了60px, 底部给padding-bottom 60px,  height:100%是给滚动效果 -->
+      <el-container style="height:100%; padding-bottom:60px">
+        <el-aside width="200px">
+          <!-- 侧边栏 -->
+          <li v-for="i in 100" :key="i">{{i}}</li>
+        </el-aside>
+        <el-main>
+          <!-- 主内容 -->
+          <li v-for="i in 100" :key="i">{{i}}</li>
+        </el-main>
+      </el-container>
+    </el-container>
+
+    <!-- <router-view></router-view> -->
   </div>
 </template>
 
@@ -10,4 +23,28 @@
 </script>
 
 <style>
+  .fullscrean {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+  .el-header {
+      background-color: #B3C0D1;
+      color: #333;
+
+  }
+    
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+
+  }
 </style>
