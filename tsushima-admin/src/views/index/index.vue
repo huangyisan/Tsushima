d010385f6d6019d72d0da309c32ec7c1df3e3d82
huangyisan <ahuangyisan@live.cn>
2020-08-02 23:52:01 +0800

    update 商品card

--- tsushima-admin/src/views/index/index.vue
+++ tsushima-admin/src/views/index/index.vue
@@ -16,6 +16,106 @@
         </el-card>
       </el-col>
     </el-row>
+
+    <!-- 店铺,订单提示| 统计图 -->
+    <el-row :gutter="20" class="mt-3">
+      <!-- 店铺 订单提示-->
+      <el-col :span="12" style="height: 370px" class="d-flex flex-column">
+        <el-card class="box-card mb-auto" shadow="never">
+          <div slot="header" class="clearfix">
+            <span>店铺及商品提示</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div class="row">
+            <div class="col-3">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+            <div class="col-3">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+            <div class="col-3">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+            <div class="col-3">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+            
+          </div>
+        </el-card>
+
+        <el-card class="box-card" shadow="never">
+          <div slot="header" class="clearfix">
+            <span>交易提示</span>
+            <el-button style="float: right; padding: 3px 0" type="text">交易提示</el-button>
+          </div>
+          <div class="row">
+            <div class="col-2">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+            <div class="col-2">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+            <div class="col-2">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+            <div class="col-2">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+            <div class="col-2">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+            <div class="col-2">
+              <button class="btn-light btn w-100">
+                <h4 mb-1>50</h4>
+                <small class="text-muted">出售中</small>
+              </button>
+            </div>
+          </div>
+        </el-card>
+      </el-col>
+
+      <!--  统计图 -->
+      <el-col :span="12">
+        <el-card class="box-card" shadow="never" style="height:370px">
+          <div slot="header" class="clearfix">
+            <span>统计图</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <div class="text item">
+            统计图
+          </div>
+            
+        </el-card>
+      </el-col>
+    </el-row>
+
   </div>
 </template>
 
