f557b5db2aec44a2b1f85b02e1cf50284f7f51e9
huangyisan <ahuangyisan@live.cn>
2020-08-03 23:04:21 +0800

    update

--- tsushima-admin/src/views/index/index.vue
+++ tsushima-admin/src/views/index/index.vue
@@ -20,42 +20,23 @@
     <!-- 店铺,订单提示| 统计图 -->
     <el-row :gutter="20" class="mt-3">
       <!-- 店铺 订单提示-->
-      <el-col :span="12" style="height: 370px" class="d-flex flex-column">
-        <el-card class="box-card mb-auto" shadow="never">
+      <el-col :span="12" style="height: 370px; justify-content: space-between" class="d-flex flex-column">
+        <el-card class="box-card mb-auto" shadow="never" v-for="(item,index) in tips" :key="index">
           <div slot="header" class="clearfix">
-            <span>店铺及商品提示</span>
-            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            <span>{{item.title}}</span>
+            <el-button style="float: right; padding: 3px 0" type="text">{{item.desc}}</el-button>
           </div>
           <div class="row">
-            <div class="col-3">
+            <div :class="item.list.length | getCol" v-for="(iten, indey) in item.list" :key="indey">
               <button class="btn-light btn w-100">
-                <h4 mb-1>50</h4>
-                <small class="text-muted">出售中</small>
+                <h4 mb-1>{{iten.value}}</h4>
+                <small class="text-muted">{{iten.name}}</small>
               </button>
-            </div>
-            <div class="col-3">
-              <button class="btn-light btn w-100">
-                <h4 mb-1>50</h4>
-                <small class="text-muted">出售中</small>
-              </button>
-            </div>
-            <div class="col-3">
-              <button class="btn-light btn w-100">
-                <h4 mb-1>50</h4>
-                <small class="text-muted">出售中</small>
-              </button>
-            </div>
-            <div class="col-3">
-              <button class="btn-light btn w-100">
-                <h4 mb-1>50</h4>
-                <small class="text-muted">出售中</small>
-              </button>
-            </div>
-            
+            </div>      
           </div>
         </el-card>
 
-        <el-card class="box-card" shadow="never">
+        <!-- <el-card class="box-card" shadow="never">
           <div slot="header" class="clearfix">
             <span>交易提示</span>
             <el-button style="float: right; padding: 3px 0" type="text">交易提示</el-button>
@@ -101,7 +82,7 @@
         </el-card>
       </el-col>
 
-      <!--  统计图 -->
+       统计图
       <el-col :span="12">
         <el-card class="box-card" shadow="never" style="height:370px">
           <div slot="header" class="clearfix">
@@ -112,7 +93,7 @@
             统计图
           </div>
             
-        </el-card>
+        </el-card> -->
       </el-col>
     </el-row>
 
@@ -136,7 +117,39 @@ export default {
         {
           icon: "el-icon-delete-solid",num: 1130,desc: "本月金额(元)", color:"bg-warning"
         },
-      ]
+      ],
+      tips: [
+        { 
+          title:"店铺及商品提示",
+          desc:"需要关注店铺信息及处理注意事项",
+          list: [
+            { name: "出售中", value: 64},
+            { name: "出售中", value: 64},
+            { name: "出售中", value: 64},
+            { name: "出售中", value: 64},
+
+          ]
+        },
+        { 
+          title:"店铺及商品提示",
+          desc:"需要关注店铺信息及处理注意事项",
+          list: [
+            { name: "出售中", value: 64},
+            { name: "出售中", value: 64},
+            { name: "出售中", value: 64},
+            { name: "出售中", value: 64},
+            { name: "出售中", value: 64},
+            { name: "出售中", value: 64},
+
+          ]
+        }
+      ],
+    }
+  },
+  filters: {
+    // 动态获取行数
+    getCol(value) {
+      return `col-${12 / value}`
     }
   }
 }
