c7c457256cc652d786a4c466995f7b38c74ae030
huangyisan <ahuangyisan@live.cn>
2020-08-04 16:49:03 +0800

    相册搜索框

--- tsushima-admin/src/views/image/index.vue
+++ tsushima-admin/src/views/image/index.vue
@@ -1,6 +1,22 @@
 <template>
   <el-container style="position: absolute; top: 74px; bottom: 0; left: 0; right: 0;">
-    <el-header>Header</el-header>
+    <el-header  class="d-flex align-items-center border-bottom">
+      <div class="d-flex mr-auto">
+        <el-select v-model="value" placeholder="请选择" size="mini">
+          <el-option>
+          </el-option>
+          <el-option>
+          </el-option>
+        </el-select>
+        <el-input  size="mini"></el-input>
+        <el-button type="primary" size="mini">搜索</el-button>
+      </div>
+        <el-button type="success" size="medium">创建相册</el-button>
+        <el-button type="warning" size="medium">上传图片</el-button>
+
+
+
+    </el-header>
     <el-container>
       <el-aside width="200px" style="position: absolute; top:60px; left:0; bottom: 60px">
       <!-- <el-aside width="200px"> -->
@@ -27,7 +43,7 @@ export default Vue.extend({
 </script>
 
 <style>
- .el-header, .el-footer {
+ .el-footer {
     background-color: #B3C0D1;
     color: #333;
   }
